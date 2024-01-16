79c89b2bd8de4a42f3b5b8582aa33b8688dcb00d
Dmitrii.Kosarev <Kakadu@pm.me>
2024-01-16 20:24:41 +0300

    WIP
    Signed-off-by: Dmitrii.Kosarev <Kakadu@pm.me>

--- json/lama_on_node.ml
+++ json/lama_on_node.ml
@@ -4,8 +4,7 @@ open Js_of_ocaml.Firebug
 (* let () = Firebug.console##log (Js.string "asdf") *)
 module Predefined = struct
   module Expr = struct
-    (* let _ = [%blob "lama_on_node.ml"] *)
-    let one = [%blob "1.lama.json"]
+    let one = [%blob "1write.bc.json"]
   end
 
   module Funs = struct
@@ -27,9 +26,38 @@ let eval_bc_string contents =
 
 let __ () = eval_bc_string Predefined.Other.append
 
+let clear_output_area () =
+  (Dom_html.getElementById_exn "output_area")##.textContent := Js.null
+
 let get_stru_text_exn () =
-  Dom_html.getElementById_coerce "my-text-area" Dom_html.CoerceTo.textarea
+  Dom_html.getElementById_coerce "input_area" Dom_html.CoerceTo.textarea
   |> Option.get
+
+let run_clicked () =
+  clear_output_area ();
+  let textarea = get_stru_text_exn () in
+  (* (Dom_html.getElementById_exn "output_area")##.textContent
+     := Js.some textarea##.value *)
+  let j = Yojson.Safe.from_string (Js.to_string textarea##.value) in
+  let bc = LibSerialize.json_to_bytecode j in
+  let rez : int list = SM.run bc [] in
+  (Dom_html.getElementById_exn "output_area")##.textContent
+  := Js.some
+     @@ Js.string
+          (Format.asprintf "Result: @[%a@]\n%!"
+             Format.(pp_print_list pp_print_int)
+             rez)
+
+let () =
+  (Dom_html.getElementById_exn "send1")##.onclick
+  := Dom.handler (fun _ ->
+         run_clicked ();
+         Js._true)
+
+let () =
+  let input_area = Dom_html.createTextarea Dom_html.document in
+  input_area##setAttribute (Js.string "id") (Js.string "input_area");
+  Dom.appendChild (Dom_html.getElementById_exn "left") input_area
 
 let () =
   let examples =
@@ -60,3 +88,10 @@ let () =
         Js._true);
   _combo##.selectedIndex := 0;
   on_change ()
+(*
+   let () =
+     let area = get_stru_text_exn () in
+     area##.onchange :=
+       Dom_html.handler (fun _ ->
+           console##log (Js.string "json changed");
+           Js._true) *)
